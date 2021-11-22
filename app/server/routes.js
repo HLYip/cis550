f76ea321bb445cb7b03c702c0109cb99a32051a6
Haolin  Ye <haolinye@lawn-143-215-95-128.lawn.gatech.edu>
2021-11-22 17:45:17 -0500

    query

--- app/server/routes.js
+++ app/server/routes.js
@@ -13,9 +13,6 @@ const connection = mysql.createConnection({
 connection.connect();
 
 
-// ********************************************
-//            SIMPLE ROUTE EXAMPLE
-// ********************************************
 
 // Route 1 (handler)
 async function hello(req, res) {
@@ -27,56 +24,47 @@ async function hello(req, res) {
     }
 }
 
+async function stateinfo(req,res){
 
-// ********************************************
-//                  WARM UP 
-// ********************************************
+    const state = req.query.state
+    
 
-// Route 2 (handler)
-async function jersey(req, res) {
-    const colors = ['red', 'blue', 'white']
-    const jersey_number = Math.floor(Math.random() * 20) + 1
-    const name = req.query.name ? req.query.name : "player"
-
-    if (req.params.choice === 'number') {
-        // TODO: TASK 1: inspect for issues and correct 
-        res.json({ message: `Hello, ${name}!`, lucky_number: jersey_number })
-    } else if (req.params.choice === 'color') {
-        var lucky_color_index = Math.floor(Math.random() * 2) + 1;
-        // TODO: TASK 2: change this or any variables above to return only 'red' or 'blue' at random (go Quakers!)
-        res.json({ message: `Hello, ${name}!`, jersey_color: colors[lucky_color_index] })
-    } else {
-        // TODO: TASK 3: inspect for issues and correct
-        res.json({ message: `Hello,${name}, we like your jersey!` })
-    }
-}
-
-// ********************************************
-//               GENERAL ROUTES
-// ********************************************
-//a
-
-// Route 3 (handler)
-async function all_matches(req, res) {
-    // TODO: TASK 4: implement and test, potentially writing your own (ungraded) tests
-    // We have partially implemented this function for you to 
-    // parse in the league encoding - this is how you would use the ternary operator to set a variable to a default value
-    // we didn't specify this default value for league, and you could change it if you want! 
-    // in reality, league will never be undefined since URLs will need to match matches/:league for the request to be routed here... 
-    const league = req.params.league ? req.params.league : 'D1'
-    // use this league encoding in your query to furnish the correct results
-
-    if (req.query.page && !isNaN(req.query.page)) {
-        // This is the case where page is defined.
-        // The SQL schema has the attribute OverallRating, but modify it to match spec! 
-        // TODO: query and return results here:
-   
-    } else {
-        // we have implemented this for you to see how to return results by querying the database
-        connection.query(`SELECT MatchId, Date, Time, HomeTeam AS Home, AwayTeam AS Away, FullTimeGoalsH AS HomeGoals, FullTimeGoalsA AS AwayGoals  
-        FROM Matches 
-        WHERE Division = '${league}'
-        ORDER BY HomeTeam, AwayTeam`, function (error, results, fields) {
+    
+    if(req.query.state){
+        connection.query(`with temp as (
+            select r.county, count(c.category) as counts, avg(stars) as avg_stars
+            from Restaurants r
+                     join Categories c on r.business_id = c.business_id
+            where r.state = '${state}'
+            group by r.county
+        ), health_index as (
+            select county, sum(vacc_count),
+        Rank() OVER
+            (ORDER BY avg(vacc_pct) DESC) as vac_rate_rank,
+        Rank() OVER
+            (ORDER BY avg(pos_pct) ASC) as low_pos_rate_rank
+        from Health
+        where state_abbr = '${state}'
+        and vacc_pct is not null and pos_pct is not null
+        group by county
+        )
+        select health_index.county, temp.avg_stars as average_rating,
+        case when temp.counts >10 then 'plentiful choices'
+        when temp.counts >5 and temp.counts<=10 then 'several choices'
+        when temp.counts is null then 'choices unknown'
+        else 'few choices'
+        end as num_category_county,
+        case when low_pos_rate_rank < 0.3* (select count(distinct(county)) from Health where state_abbr = '${state}') then 'safe county'
+        when low_pos_rate_rank <= 0.7* (select count(distinct(county)) from Health where state_abbr = '${state}') then 'fine county'
+        else 'risky county'
+        end as positive_level,
+        case when vac_rate_rank < 0.3* (select count(distinct(county)) from Health where state_abbr = '${state}') then 'highly vaccinated'
+        when vac_rate_rank < 0.7* (select count(distinct(county)) from Health where state_abbr = '${state}') then 'medium vaccinated'
+        else 'low vaccinated'
+        end as vaccination_level
+        from health_index left join temp on health_index.county = temp.county
+        group by county
+        order by health_index.county asc`, function (error, results, fields)  {
 
             if (error) {
                 console.log(error)
@@ -85,68 +73,73 @@ async function all_matches(req, res) {
                 res.json({ results: results })
             }
         });
+        
+    }else{
+        res.send('bye')
     }
 }
 
-// Route 4 (handler)
-async function all_players(req, res) {
-    // TODO: TASK 5: implement and test, potentially writing your own (ungraded) tests
+
+
+async function restlocation (req, res){
+
+    const lat = req.query.lat
+    const long = req.query.long
+    const category = req.query.category
+
+    if(req.query.lat && req.query.long && req.query.category){
+        connection.query(`with a as (
+            select * from Categories natural join Restaurants
+        )
+        select a.business_id,a.name,a.stars,
+        case when stars >4 and review_count > 20 then 'highly recommend'
+        when stars >3 and review_count > 5 then 'recommend'
+        else 'not recommend'
+        end as recommendation
+        
+        from a
+        where abs(a.r_lat - ${lat}) <= 0.01  
+        and abs(a.r_long - (${long})) <= 0.01  
+        and a.category Like '%${category}%' 
+        order by a.stars,a.review_count desc `,function (error, results, fields) {
+
+           if (error) {
+               console.log(error)
+               res.json({ error: error })
+           } else if (results) {
+               res.json({ results: results })
+               
+               
+           }
+       });
+
+}else{
+    connection.query(`select * from Restaurants
+    where stars>=4 and is_open = 1
+    order by RAND()
+    limit 8`,function (error, results, fields) {
+
+       if (error) {
+           console.log(error)
+           res.json({ error: error })
+       } else if (results) {
+           res.json({ results: results })
+           
+           
+       }
+   });
+
     
-
-    return res.json({error: "Not implemented"})
 }
+    }
 
 
-// ********************************************
-//             MATCH-SPECIFIC ROUTES
-// ********************************************
-
-// Route 5 (handler)
-async function match(req, res) {
-    // TODO: TASK 6: implement and test, potentially writing your own (ungraded) tests
-    return res.json({error: "Not implemented"})
-}
-
-// ********************************************
-//            PLAYER-SPECIFIC ROUTES
-// ********************************************
-
-// Route 6 (handler)
-async function player(req, res) {
-    // TODO: TASK 7: implement and test, potentially writing your own (ungraded) tests
-    return res.json({error: "Not implemented"})
-}
 
 
-// ********************************************
-//             SEARCH ROUTES
-// ********************************************
 
-// Route 7 (handler)
-async function search_matches(req, res) {
-    // TODO: TASK 8: implement and test, potentially writing your own (ungraded) tests
-    // IMPORTANT: in your SQL LIKE matching, use the %query% format to match the search query to substrings, not just the entire string
-
-    return res.json({error: "Not implemented"})
-
-
-}
-
-// Route 8 (handler)
-async function search_players(req, res) {
-    // TODO: TASK 9: implement and test, potentially writing your own (ungraded) tests
-    // IMPORTANT: in your SQL LIKE matching, use the %query% format to match the search query to substrings, not just the entire string
-    
-    return res.json({error: "Not implemented"})
-}
 
 module.exports = {
     hello,
-    jersey,
-    all_matches,
-    all_players,
-    match,
-    player,
-    search_matches,
-    search_players
+    stateinfo,
+    restlocation
 }
